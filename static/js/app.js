7146a91879f1fa8d5149aefdf9365821e581d199
Emil Rodulfo <emilrodulfo@gmail.com>
2019-01-09 01:01:19 -0500

    submission

--- static/js/app.js
+++ static/js/app.js
@@ -29,12 +29,12 @@ var submit = d3.select("#filter-btn");
 
 submit.on("click", function() {
 
-
     // Prevent the page from refreshing
     d3.event.preventDefault();
 
-    var tbodyRows = d3.selectAll("tbody>tr");
-    tbodyRows.remove();
+    // Remove current tbody values
+    var tbodyCells = d3.selectAll("tbody>td");
+    tbodyCells.remove();
 
     // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
@@ -42,17 +42,41 @@ submit.on("click", function() {
     // Get the value property of the input element
     var inputValue = inputElement.property("value");
 
+    // Filter through the original table data value based on the search filter value
     var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
     console.log(filteredData);
-    
-    //document.getElementById("tbody").innerHTML = "";
 
-    // Iterate through each array in the data
+    // Iterate through each array in the data from the filter data and post in the table
     filteredData.forEach((sightingsReport) => {
       var row = tbody.append("tr");
       Object.entries(sightingsReport).forEach(([key, value]) => {
         var cell = tbody.append("td");
         cell.text(value);
+    });
+  });
+});
+
+
+//RESET DATA
+
+// Select the submit button
+var reset = d3.select("#reset-btn");
+
+reset.on("click", function() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Remove current tbody values
+  var tbodyCells = d3.selectAll("tbody>td");
+  tbodyCells.remove();
+
+  // Iterate through each array in the data
+  tableData.forEach((sightingsReport) => {
+    var row = tbody.append("tr");
+    Object.entries(sightingsReport).forEach(([key, value]) => {
+      var cell = tbody.append("td");
+      cell.text(value);
     });
   });
 });
